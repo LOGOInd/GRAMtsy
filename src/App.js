5d625bfd5944e6e90da475934afde62893148f4c
proRamLOGO <kapilgupta547@outlook.com>
2021-02-04 19:20:17 +0530

    gp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
 import logo from './logo.svg';
-import './styles/App.css';
+// import './styles/App.css';
 import Header from './Header';
 import Home from './Home';
+import Checkout from './Checkout';
 import Product from './Product';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+
       <Header/>
-      <Home/>
-    </div>
+
+      <Switch>
+
+        <Route path="/checkout" >
+          <div className="App">
+            <Checkout/>
+          </div>
+        </Route>
+
+        <Route path="/" >
+          <div className="App">
+            <Home/>
+          </div>
+        </Route>
+
+      </Switch>
+
+    </Router>
   );
 }
 
---
+++ src/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/Checkout.css'
+
+function Checkout() {
+    return (
+        <div className="checkout" >
+
+            <h1>Checkout</h1>
+
+        </div>
+    )
+}
+
+export default Checkout
